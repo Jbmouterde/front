cedef15d5c1e7c093684b59187f42b6b7029e41e
Jb mout <jeanbaptiste.mouterde@kedgebs.com>
2018-05-11 14:19:35 +0200

    map fixed!!!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import{ HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -48,6 +48,7 @@ import { GooglePlaceModule } from "ngx-google-places-autocomplete";
     GooglePlaceModule,
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     HttpClientModule,
     FormsModule,
     ChartsModule,
